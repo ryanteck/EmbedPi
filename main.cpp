1c671c54b2afbc08b3b4391eeb0cd30ffd8afdda
Ryan Walmsley <ryanteck@gmail.com>
2013-07-11 11:22:11 +0100

    Upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include .arduPi.h.
+// Modified Template
+
+
+void setup() {
+
+//Normal setup code here
+
+}
+
+void loop() {
+//Normal Arduino Loop Here
+
+}
+
+
+
+int main (){
+
+setup();
+
+while(1){
+
+loop();
+
+}
+
+return (0);
+
+}
+
